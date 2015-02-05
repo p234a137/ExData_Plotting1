42d3fd8b2b3d4f57a049a10a0ff40bd4c1a4e350
R2 D2 <p234a137@gmail.com>
2015-02-04 21:43:36 -0800

    recommit plot1.R

---
+++ plot1.R
@@ -0,0 +1,33 @@
+
+
+# Date: Date in format dd/mm/yyyy
+# Time: time in format hh:mm:ss
+# Global_active_power: household global minute-averaged active power (in kilowatt)
+# Global_reactive_power: household global minute-averaged reactive power (in kilowatt)
+# Voltage: minute-averaged voltage (in volt)
+# Global_intensity: household global minute-averaged current intensity (in ampere)
+# Sub_metering_1: energy sub-metering No. 1 (in watt-hour of active energy). It corresponds to the kitchen, containing mainly a dishwasher, an oven and a microwave (hot plates are not electric but gas powered).
+# Sub_metering_2: energy sub-metering No. 2 (in watt-hour of active energy). It corresponds to the laundry room, containing a washing-machine, a tumble-drier, a refrigerator and a light.
+# Sub_metering_3: energy sub-metering No. 3 (in watt-hour of active energy). It corresponds to an electric water-heater and an air-conditioner.
+
+
+# calculate an estimate of the memory usage in bytes, assuming 8 bytes / numeric
+no_of_columns = 9
+no_of_rows = 2075259
+memory_required = no_of_columns * no_of_rows * 8 
+cat("calculated memory required in bytes = ", memory_required/(1024*1024), units = "MB")
+cat("\nfile size = ", file.info("./household_power_consumption.txt")$size/(1024*1024))
+
+colClasses = c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric")
+householdPowerConsumption <- read.table("./household_power_consumption.txt", header = T, na.strings = "?", sep=";", quote="", comment.char = "")
+cat("\nactual object size = ", format(object.size(householdPowerConsumption), units = "MB"))
+
+householdPowerConsumption$datetime <- as.POSIXct(strptime(
+  paste(householdPowerConsumption$Date, householdPowerConsumption$Time, sep=" ")
+  , "%d/%m/%Y %H:%M:%S"))
+householdPowerConsumption$Date <- as.Date(strptime(householdPowerConsumption$Date, "%d/%m/%Y"))
+
+# select dates between "2007-02-01" and "2007-02-02"
+householdPowerConsumption <- householdPowerConsumption[ (householdPowerConsumption$Date >= "2007-02-01" & householdPowerConsumption$Date <= "2007-02-02"), ]
+
+hist(householdPowerConsumption$Global_active_power, col="red")
